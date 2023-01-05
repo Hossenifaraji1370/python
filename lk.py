c387a0da3330aa841225c219e29949d17669bf88
Hossenifaraji1370 <122002273+Hossenifaraji1370@users.noreply.github.com>
2023-01-05 10:20:08 +0330

    Add files via upload

--- lk.py
+++ lk.py
@@ -1,50 +1,5 @@
-xx = 100
-xx = xx + 4
-print (xx)
-uu = 40 / 10
-print (uu)
-pp = uu * 50
-print (pp)
-ddd = 1 + 4
-print (ddd)
-eee = 'hello ' + 'there'
-print (eee)
-print(float(99) + 100)
-print(99.0 / 100.0)
-
-x = 5
-if x == 5 : 
-    print('Equals 5')
-if x > 4 : 
-   print('Greater than 4')
-if  x >= 5 :
-    print('Greater than or Equals 5')
-if x < 6 : print('Less than 6') 
-if x <= 5 :
-    print('Less than or Equals 5')
-if x != 6 :
-    print('Not equal 6')
-
-x = 42
-if x > 1 :
-    print('More than one')
-    if x < 100 : 
-        print('Less than 100') 
-print('All done')
-
-x = 5
-if x > 2 :
-    print('ok')
-else :
-    print('dont')
-
-print('All done')
-
-if x < 2 :
-    print('small')
-elif x < 10 :
-    print('Medium')
-else :
-    print('LARGE')
-print('All done')
-
+n = 5
+while n > 0 :
+    print('Lather')
+    print('Rinse')
+print('Dry off!')
